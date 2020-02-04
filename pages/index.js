20305b52525d7ecbbf046c747be0423d44261b30
Olga Vorozheykina <solariskind@gmail.com>
2020-02-04 17:02:14 +0200

    Add CSS deps and sample render

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-export default function Index() {
+export default () => {
+  const sampleFloss = {
+    brand: "DMC",
+    color: 3713,
+    r: 255,
+    g: 226,
+    b: 226
+  };
+
   return (
-    <div>
-      <p>Hello Next.js</p>
+    <div className="App">
+      <div className="flex">
+        <p className="font-sans text-4xl">{sampleFloss.brand}</p>
+        <p>{sampleFloss.color}</p>
+        <div
+          style={{
+            width: "100px",
+            height: "50px",
+            backgroundColor: `rgb(${sampleFloss.r}, ${sampleFloss.g}, ${
+              sampleFloss.b
+            })`
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
