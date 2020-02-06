af7c6f43e4f69121ab2eff123c2ae9301c1237c1
Olga Vorozheykina <solariskind@gmail.com>
2020-02-06 13:41:13 +0200

    Add styles for floss entry

--- components/FlossCard.tsx
+++ components/FlossCard.tsx
@@ -10,16 +10,19 @@ interface Floss {
 
 const FlossCard = ({ brand, color, r, g, b }: Floss) => {
   return (
-    <div className="flex justify-between w-1/5">
-      <p className="font-sans">{brand}</p>
-      <p>{color}</p>
+    <div className="flex m-4 p-1 w-1/5 justify-between border border-indigo-800">
       <div
+        className="w-16 h-16 relative"
         style={{
-          width: "100px",
-          height: "50px",
-          backgroundColor: `rgb(${r}, ${g}, ${b})`
+          backgroundColor: `rgb(${r}, ${g}, ${b})`,
+          right: "15px"
         }}
       />
+      <div className="flex flex-col justify-around">
+        <p className="font-sans tracking-wide font-medium">{brand}</p>
+        <p className="font-sans">{color}</p>
+      </div>
+      <p className="text-4xl">0</p>
     </div>
   );
 };
